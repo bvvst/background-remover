714f53424734a8e2fd4c1086da3bb8b667820945
luke <lukeimbing@gmail.com>
2022-11-21 00:39:41 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bvvst/background-remover
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	_ "image/jpeg"
+	"image/png"
+	"math"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	// Fetch Image Response
+	res, err := http.Get("https://cdn.discordapp.com/attachments/951265974961721404/1044067933791457280/IMG_5647.jpg")
+	if err != nil || res.StatusCode != 200 {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+
+	// Decode Image
+	decodedImage, _, err := image.Decode(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Make Image RGBA Image (to allow for transparency)
+	img := image.NewRGBA(decodedImage.Bounds())
+	size := img.Bounds().Size()
+	for x := 0; x < size.X; x++ {
+		for y := 0; y < size.Y; y++ {
+			img.Set(x, y, decodedImage.At(x, y))
+		}
+	}
+
+	// Remove the background
+	RemoveBackground(img)
+
+	duration := time.Since(start)
+	fmt.Println(duration)
+
+	outFile, _ := os.Create("output/changed.png")
+	defer outFile.Close()
+	png.Encode(outFile, img)
+}
+
+func RemoveBackground(image *image.RGBA) {
+	maxX := image.Bounds().Max.X
+	maxY := image.Bounds().Max.Y
+	targetColor := Fill(0, 0, color.RGBA{0, 0, 0, 0}, image)
+	FillWithTargetColor(targetColor, maxX-1, maxY-1, color.RGBA{0, 0, 0, 0}, image)
+	FillWithTargetColor(targetColor, 0, maxY-1, color.RGBA{0, 0, 0, 0}, image)
+	FillWithTargetColor(targetColor, maxX-1, 0, color.RGBA{0, 0, 0, 0}, image)
+}
+
+func Fill(x int, y int, newColor color.Color, image *image.RGBA) color.Color {
+	targetColor := image.At(x, y)
+
+	if image.At(x, y) == targetColor {
+		image.SetRGBA(x, y, color.RGBA{0, 0, 0, 0})
+		if x+1 <= image.Bounds().Max.X {
+			FillWithTargetColor(targetColor, x+1, y, newColor, image)
+		}
+		if x-1 >= 0 {
+			FillWithTargetColor(targetColor, x-1, y, newColor, image)
+		}
+		if y+1 <= image.Bounds().Max.Y {
+			FillWithTargetColor(targetColor, x, y+1, newColor, image)
+		}
+		if y-1 >= 0 {
+			FillWithTargetColor(targetColor, x, y-1, newColor, image)
+		}
+	}
+
+	return targetColor
+}
+
+func FillWithTargetColor(targetColor color.Color, x int, y int, newColor color.Color, image *image.RGBA) {
+	rx, gx, bx, ax := image.At(x, y).RGBA()
+
+	tr, tg, tb, ta := targetColor.RGBA()
+
+	a := math.Abs(float64(rx)-float64(tr)) + math.Abs(float64(gx)-float64(tg)) + math.Abs(float64(bx)-float64(tb)) + math.Abs(float64(ax)-float64(ta))
+
+	if a < 30000 {
+		image.SetRGBA(x, y, color.RGBA{0, 0, 0, 0})
+		if x+1 < image.Bounds().Max.X {
+			FillWithTargetColor(targetColor, x+1, y, newColor, image)
+		}
+		if x-1 > 0 {
+			FillWithTargetColor(targetColor, x-1, y, newColor, image)
+		}
+		if y+1 < image.Bounds().Max.Y {
+			FillWithTargetColor(targetColor, x, y+1, newColor, image)
+		}
+		if y-1 > 0 {
+			FillWithTargetColor(targetColor, x, y-1, newColor, image)
+		}
+	}
+}
